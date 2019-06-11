ead2f9142850aeb7ed1dec7946eba4d0ea1100b5
13JonathanDavid <48808640+13JonathanDavid@users.noreply.github.com>
2019-06-10 20:23:45 -0400

    added CRUD some custom middleware added for verification
    TODO
    checking final MVP, code may or may not run.

--- index.js
+++ index.js
@@ -8,49 +8,94 @@ const port = 3333;
 const server = express();
 server.use(express.json());
 
-server.post('/api/users',[], (req,res)=> {
-    const {name, bio} = req.body;
+server.post('/api/users', (req,res)=>{
+    if(!typeof req.body.name || req.body.name =="" ||
+    !typeof req.body.bio || req.body.bio !="")
+        return res.status(400, "errorMessage: Please provide name and bio for the user.");
+}), (req, res) => {
+    const { name, bio } = req.body;
     console.log({
-        name : name,
-        bio : bio,
+        name: name,
+        bio: bio,
     })
     db.insert({
-        name : name,
-        bio : bio})
-    .then( () => { 
-        res.status(200).json({'success':true})})
-    .catch(() => { 
-        res.status(500).json({'success':false});
-    });
+        name: name,
+        bio: bio
+    })
+        .then((user) => {
+            res.status(201).json({ 'success': true, user:user })
+        })
+        .catch(() => {
+            res.status(500).json({ 'success': false });
+        });
 }
 );
 
-server.get('/api/users',[], (req,res)=> {
-       db.find()
-    .then( (data) => { 
-        res.status(200).json({'success':true, users: data})})
-    .catch(() => { 
-        res.status(500).json({'success':false});
-    });
+server.get('/api/users', [], (req, res) => {
+    db.find()
+        .then((data) => {
+            res.status(200).json({ 'success': true, users: data })
+        })
+        .catch(() => {
+            res.status(500).json({ 'success': false });
+        });
 });
 
-server.get('/api/users/:id',[], (req,res)=> {
-    const {id} = req.params;
+server.get('/api/users/:id', [], (req, res) => {
+    const { id } = req.params;
     console.log(id);
     db.findById(id)
- .then( (data) => {
-     if (typeof data && data.length >= 1)
-        res.status(200).json({'success':true, user: data});
-    else  
-        res.status(403).json({'success':false, user: data});
- })
- .catch(() => { 
-     res.status(500).json({'success':false});
- });
+        .then((data) => {
+            if (typeof data)
+                res.status(200).json({ 'success': true, user: data });
+            else
+                res.status(403).json({ 'success': false, user: data });
+        })
+        .catch(() => {
+            res.status(500).json({ 'success': false });
+        });
 });
 
+server.delete('/api/users/:id', [], (req, res) => {
+    const { id } = req.params;
+    db.findById(id).then((condemnedUser) => {
+        const usr = condemnedUser;
+        db.remove(id)
+            .then((data) => {
+                if (typeof data && data == true && usr) {
+                    res.status(200).json({ 'success': true, user: usr });
+                }
+                else {
+                    res.status(403).json({ 'success': false });
+                }
+            })
+    })
+        .catch(() => {
+            res.status(500).json({ 'success': false });
+        });
+});
 
-
-server.listen(port,()=>{
+server.put('/api/users/:id', [], (req, res) => {
+    const obj = { ...req.body }
+    console.log(obj);
+    const { id } = req.params;
+    console.log(id);
+    db.findById(id).then((user) => {
+        db.update(id, obj).then((data) => {
+x = { }
+            if (typeof data && data == true) {
+                const updateUser = {...user,...obj};
+                res.status(200).json({ 'success': true, user:{...updateUser} });
+            }
+            else {
+                res.status(403).json({ 'success': false });
+            }
+        })
+    })
+        .catch(() => {
+            res.status(500).json({ 'success': false });
+        });
+});
+server.listen(port, () => {
     console.log(`I am listening on part ${port}`);
 });
